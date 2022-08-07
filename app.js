e136da2e2d02aa6aa2460219f5f64f6d4f621276
Claire Bourdon <cebourdon625@gmail.com>
2022-08-07 14:01:21 -0400

    changed port value

--- app.js
+++ app.js
@@ -96,7 +96,7 @@ app.use('/', require('./routes/index'))
 app.use('/auth', require('./routes/auth'))
 app.use('/stories', require('./routes/stories'))
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8000
 
 app.listen(
     PORT, 
