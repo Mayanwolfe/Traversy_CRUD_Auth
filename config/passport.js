a84063c2e07b840f88aed3931834d21e632281f4
Sean Poole <spoole5289@gmail.com>
2023-03-29 21:19:56 -0700

    update deprecated methods

--- config/passport.js
+++ config/passport.js
@@ -34,9 +34,12 @@ module.exports = function(passport) {
     passport.serializeUser((user,done) => {
         done(null,user.id)
     });
-    passport.deserializeUser((id,done) => {
-        User.findById(id,(err, user) => {
-            done(err, user)
-        })
-    })
+    passport.deserializeUser(async (id, done) => {
+        try {
+            const user = await User.findById(id);
+            done(null, user);
+        } catch (err) {
+            done(err);
+        }
+    });
 }
--- routes/stories.js
+++ routes/stories.js
@@ -116,7 +116,7 @@ router.put('/:id', ensureAuth, async (req,res) => {
 //@Route DELETE /stories/:id
 router.delete('/:id', ensureAuth, async (req,res) => {
     try {
-        await Story.remove({ _id: req.params.id })
+        await Story.deleteOne({ _id: req.params.id })
         res.redirect('/dashboard')
     } catch (err) {
         console.error(err)
